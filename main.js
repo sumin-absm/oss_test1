05d1dcb7aa6c7e160c2401143a79a957cb4477c5
sumin-absm <jangsumin21@gmail.com>
2024-10-02 10:14:36 +0900

    implement add

---
+++ main.js
@@ -0,0 +1,13 @@
+function add(a,b){
+    return a+b;
+}
+
+function add_from(a,b){
+    return -1;
+}
+
+let in1=5;
+let in2=10;
+
+console.log("add result: "+add(in1,in2));
+console.log("add_from result: "+add_from(in1,in2));
